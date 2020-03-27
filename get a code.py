7514a02f7dddb7792de2d1661fec3b88c837df4f
MikeyPPPP <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2020-03-27 16:44:50 -0700

    Add files via upload

---
+++ get a code.py
@@ -0,0 +1,30 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+site = 'https://github.com/MikeyPPPPPPPP/Python_google_search/blob/master/bs4_parse%20google.py'
+
+
+a = urllib.request.urlopen(site)
+
+sitehtml = a.read()
+
+soup = BeautifulSoup(sitehtml, 'lxml')#'html.parser')
+
+
+
+
+
+
+
+
+
+
+       
+table = soup.find('table')
+table_rows = table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row[1])              
+
