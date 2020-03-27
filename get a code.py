066b3a7c025680ead9e7ccac8eb2d28e76cf4495
MikeyPPPP <50926811+MikeyPPPPPPPP@users.noreply.github.com>
2020-03-27 16:45:12 -0700

    Update get a code.py

--- get a code.py
+++ get a code.py
@@ -3,22 +3,11 @@ from bs4 import BeautifulSoup
 
 site = 'https://github.com/MikeyPPPPPPPP/Python_google_search/blob/master/bs4_parse%20google.py'
 
-
 a = urllib.request.urlopen(site)
 
 sitehtml = a.read()
 
 soup = BeautifulSoup(sitehtml, 'lxml')#'html.parser')
-
-
-
-
-
-
-
-
-
-
        
 table = soup.find('table')
 table_rows = table.find_all('tr')
